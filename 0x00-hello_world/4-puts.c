c2fa3a1b49e4b4a1ad5539a02a4a032770aa7a5a
Milka <you@example.com>
2022-10-13 12:12:37 -0700

    ll

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-
-/** 
-*main - Entry point
-*Return: 0
+#include <stdio.h?
+/**
+*main - entry point
+*return: 0
 **/
-int mian(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
