174e9a34ceb78bfb5810c18a2d5cd5f0e41f8ac4
MinimoMassimo <milkabiniam@gmail.com>
2022-10-14 19:04:10 +0300

    ll

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - starting point 
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
