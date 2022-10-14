34edf8952a82ac92b3663bb206a5c007975d6615
MinimoMassimo <milkabiniam@gmail.com>
2022-10-14 17:51:13 +0300

    l

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
@@ -22,4 +22,4 @@ int main(void)
 		printf("%d is negative\n", n);
 
 	return (0);
-}	
+}
