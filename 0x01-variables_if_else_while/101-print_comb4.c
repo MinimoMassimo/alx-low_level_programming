1b4a5a645afe402d0103abfb0aa165b16c8e9612
MinimoMassimo <milkabiniam@gmail.com>
2022-10-14 20:34:02 +0300

    ll

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,27 +7,28 @@
  */
 int main(void)
 {
-        int x;
+	int x;
         int y;
 	int j;
 
-        for (x = 0; x < 10; x++)
-        {
-                for (y = x + 1; y < 10; y++)
-                {
+	for (x = 0; x < 10; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
 			for (j = y + 1; j < 10; j++)
 			{
 				putchar((x % 10) + '0');
-                        	putchar((y % 10) + '0');
-                        	if (x != 7 || y != 8 || j != 9)
-                        	{
-                                	putchar(',');
-                                	putchar(' ');
-                        	}
+				putchar((y % 10) + '0');
+				putchar((j % 10) + '0');
+				if (x != 7 || y != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-                }
-        }
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
 
