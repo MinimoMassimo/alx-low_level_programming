8a3af01549442131f8a574eb9e78bf47ed234fa7
Milka <you@example.com>
2022-10-13 18:27:47 -0700

    kk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
-*main: entry point
-*return: 0
-**/
+ * main - Entry
+ *
+ * Return: 0 always
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
