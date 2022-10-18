ea304e08c6da86720091be5f0b6a51b7e83ba446
MinimoMassimo <milkabiniam@gmail.com>
2022-10-18 08:31:12 +0300

    ll

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,12 @@
  *
  * Return: 0 always
  */
-int main (void)
+int main(void)
 {
 	char str[] = "_putchar";
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
