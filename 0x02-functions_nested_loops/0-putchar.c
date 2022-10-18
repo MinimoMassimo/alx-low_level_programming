c89290d39912a7df4f639ea878acdbbf1409930a
MinimoMassimo <milkabiniam@gmail.com>
2022-10-18 06:56:34 +0300

    ll

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+
+/**
+ * main - prints '_putchar' using _putchar library
+ *
+ * Return: 0 always
+ */
+int main (void)
+{
+	char str[] = "_putchar";
+	int i = 0;
+
+	while(i < strlen(str))
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
