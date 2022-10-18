06c335db65cbc42e8fa227876a48ce4c3719223c
MinimoMassimo <milkabiniam@gmail.com>
2022-10-18 14:39:42 +0300

    kl

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,8 +21,9 @@ void print_times_table(int n)
 	}
 	_putchar('0');
 	_putchar('\n');
-	for (i = 1; i <= n; i++){
-		for(j = 0; j <= n; j++)
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
 		{
 			a = i * j;
 			if (j != 0)
