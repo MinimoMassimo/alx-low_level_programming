717c50400c9a7c081c0f43f3099448604b674a90
MinimoMassimo <milkabiniam@gmail.com>
2022-10-18 12:20:08 +0300

    ll

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,24 @@ void times_table(void)
 	int i, j;
 	int a = 0;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('0');
+	_putchar('\n');
+
+	for (i = 1; i < 10; i++)
+	{
+		
 		for (j = 0; j < 10; j++)
 		{
-			if (i == 0 && j == 0)
-				a = 0;
 			a = i * j;
-			_putchar(a);
+			if (a > 10)
+				_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
 			if (j != 9)
 			{
 				_putchar(',');
