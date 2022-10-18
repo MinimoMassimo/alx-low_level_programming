0425ae6132602d5fd62aa69cb0a0d1fd17e162f0
MinimoMassimo <milkabiniam@gmail.com>
2022-10-18 08:45:17 +0300

    ll

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		putchar('\n');
+	}
+}
