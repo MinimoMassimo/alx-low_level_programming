6746139b61fd4ddb015b9de8b2dedf462f550be3
MinimoMassimo <milkabiniam@gmail.com>
2022-10-19 23:28:30 +0300

    ll

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints sum of even-valued terms of fibonacci sequence < 4000000
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	long int fib[34];
+	long int sum = 2;
+
+	fib[0] = 1;
+	fib[1] = 2;
+	for (i = 2; i < 34; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+		if (fib[i] % 2 == 0)
+			sum += fib[i];
+	}
+	printf("%li\n", sum);
+	return (0);
+}
