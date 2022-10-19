868700bf1a3cdc3fe64637e4203d7e903e69af1d
MinimoMassimo <milkabiniam@gmail.com>
2022-10-19 22:56:03 +0300

    kk

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - computes the sum of all multiples of 3 or 5 < 1024
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	return (0);
+}
