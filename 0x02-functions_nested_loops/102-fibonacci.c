060df0a292a29639f467a93b24c2354486a41e29
MinimoMassimo <milkabiniam@gmail.com>
2022-10-19 23:22:18 +0300

    ll

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int fib[50];
+	long int fib[50];
 	int i;
 
 	fib[0] = 1;
 	fib[1] = 2;
-	printf("%d, ", fib[0]);
-	printf("%d, ", fib[1]);
+	printf("%li, ", fib[0]);
+	printf("%li, ", fib[1]);
 	for (i = 2; i < 50; i++)
 	{
-		fib[i] = (long int)fib[i - 1] + fib[i - 2];
+		fib[i] = fib[i - 1] + fib[i - 2];
 		if (i != 49)
 			printf("%li, ", fib[i]);
 		else
