a596acab121a8c0ebe18da116c17fd5220a5dafa
MinimoMassimo <milkabiniam@gmail.com>
2022-10-24 18:08:28 +0300

    kk

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; a[i] != '\0';)
+		i++;
+	if (i == n)
 	{
-		printf("%d, ", a[i]);
+		for (i = 0; i < (n - 1); i++)
+		{
+			printf("%d, ", a[i]);
+		}
 	}
 	printf("%d\n", a[n - 1]);
 }
