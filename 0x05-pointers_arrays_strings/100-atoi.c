58c143a789e82e94064bacc5cc212866dba54ac4
MinimoMassimo <milkabiniam@gmail.com>
2022-10-24 22:15:59 +0300

    k

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to int
+ * @s: string to be converted
+ * Return: converted string into int
+ */
+int _atoi(char *s);
+{
+	int res = 0;
+	int i, a;
+
+	if (*s < 48 || *s > 57)
+		return (0);
+	if (s[0] == '-')
+	{
+		a = 1;
+		s++;
+	}
+	if (s[0] == '+')
+		s++;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (*s < 48 || *s > 57)
+			return(0);
+		res = res * 10 + str[i] - '0';
+	}
+	if (a == 1)
+		res *= -1;
+	return (res);
+}
