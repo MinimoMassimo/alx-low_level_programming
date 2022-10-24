afc5395ee4f05df27bbda6c3ea4ba7e3c39b9d2e
MinimoMassimo <milkabiniam@gmail.com>
2022-10-24 14:36:28 +0300

    l

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string to be processed
+ * Return: nothin
+ */
+void rev_string(char *s)
+{
+	char a = *s;
+	int size = sizeof(a) / sizeof(a[0]);
+
+	_putchar(size);
+}
