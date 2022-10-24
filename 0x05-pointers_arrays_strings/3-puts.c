b25f14c5d201122689276da5213642dd32d50963
MinimoMassimo <milkabiniam@gmail.com>
2022-10-24 13:43:12 +0300

    l

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar('%c', *str);
+		*str++;
+	}
+	_putchar('\n');
