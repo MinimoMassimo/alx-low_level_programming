ea93b9ec0c1705fc25be8dd5be28c8f89e940a38
MinimoMassimo <milkabiniam@gmail.com>
2022-10-24 17:38:20 +0300

    ll

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,14 @@ void puts_half(char *str)
 {
 	int i, size;
 
-	for (size = 0; str[size] != '\0'; size++)
+	while (str[i] != '\0')
 	{
+		size++;
 		str++;
 	}
 	for (i = (size / 2); i < size; i++)
 	{
-		_putchar(str[i]);
+		_putchar(str[i - 1]);
 	}
 	_putchar('\n');
 }
