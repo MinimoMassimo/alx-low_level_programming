d2cf8c29074405ebe79d805e520b80379da77fe8
MinimoMassimo <milkabiniam@gmail.com>
2022-10-24 22:39:33 +0300

    k

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,27 +8,14 @@
 int _atoi(char *s)
 {
 	char x;
-	int res = 0;
+	long int res = 0;
 	int i, a;
 
-	while (*s != '\0' && (*s < 48 || *s > 57))
+	i = 0;
+	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
 	{
-		x = *s;
-		s++;
+		res = res * 10 + (S[i] - '0');
+		i++
 	}
-	if (*s < 48 || *s > 57)
-		return (0);
-	if (x == '-')
-	{
-		a = 1;
-	}
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (*s < 48 || *s > 57)
-			return(0);
-		res = res * 10 + s[i] - '0';
-	}
-	if (a == 1)
-		res *= -1;
 	return (res);
 }
