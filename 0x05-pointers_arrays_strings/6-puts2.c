133c359c2d1606a2c192f223686cc308ba087f3e
MinimoMassimo <milkabiniam@gmail.com>
2022-10-24 17:26:39 +0300

    ll

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,6 +5,20 @@
  * @str: string to be inputted
  * Return: nothing
  */
-void puts2(cahr *str)
+void puts2(char *str)
 {
+	int i = 0;
+	int size = -1;
 
+	while (str[i] != '\0')
+	{
+		size++;
+		i++;
+	}
+	for (i = 0; i <= size; i++)
+	{
+		if (i % 2 != 0)
+			continue;
+		_putchar(str[i]);
+	}
+}
