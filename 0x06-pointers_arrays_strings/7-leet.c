bcd76ec9a2d8a5ed95f5ba8fbc4fe3db9899f487
MinimoMassimo <milkabiniam@gmail.com>
2022-10-27 09:52:50 +0300

    l

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string into 1337
+ * @str: the string to be encoded
+ * Return: the encoded string
+ */
+char *leet(char *str)
+{
+	char rep[5][2] = {'a','4','e','3','o','0','t','7','l','1'};
+	int i, j, x;
+
+	for (x = 0; str[x]; x++)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (str[x] == rep[i][0] || str[x] == (rep[i][0] - 32))
+			{
+				str[x] = rep[i][1];
+			}
+		}
+	}
+	return (str);
+}
