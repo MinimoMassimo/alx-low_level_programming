beec4038bc5b80775bf0c9041b01170205ea031e
MinimoMassimo <milkabiniam@gmail.com>
2022-10-31 19:22:43 +0300

    l

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,12 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			return (s[i]);
+		i++;
 	}
+	if (c == '\0')
+		return ('\0');
 	return ('\0');
 }
