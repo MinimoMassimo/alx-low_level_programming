8cd2d4e68b7e4b1269e436173d82abeb08e8ded3
MinimoMassimo <milkabiniam@gmail.com>
2022-11-01 18:17:15 +0300

    l

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,6 +13,8 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == " ")
+			return (n);
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
