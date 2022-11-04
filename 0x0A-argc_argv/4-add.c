0550bbae978abe1014a9cb5d7386bef2a87ab6b0
MinimoMassimo <milkabiniam@gmail.com>
2022-11-04 14:50:08 +0300

    kk

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; j < strlen(argv[i]); j++)
+		for (j = 0; j < (int) strlen(argv[i]); j++)
 		{
 			if (argv[i][j] < 0 || argv[i][j] > 9)
 			{
