11a326d1286964d9231331a005d91a038c1800a3
MinimoMassimo <milkabiniam@gmail.com>
2022-11-04 13:50:39 +0300

    l

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,8 @@
  * main - prints the name of the program
  * Return: 0 always
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	int i;
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
