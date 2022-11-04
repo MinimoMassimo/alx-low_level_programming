c21e9bcc73e41be07a30a493c3c528639afe9f8f
MinimoMassimo <milkabiniam@gmail.com>
2022-11-04 13:43:22 +0300

    l

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%s\n", argv[0];
+	return (0);
+}
