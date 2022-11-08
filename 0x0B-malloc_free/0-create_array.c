b0f780ee46199c6799d41a87b632ba7a83859612
MinimoMassimo <milkabiniam@gmail.com>
2022-11-08 10:42:09 +0300

    l

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return ('\0');
 	str = malloc(size);
+	if (str == '\0')
+		return ('\0');
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
