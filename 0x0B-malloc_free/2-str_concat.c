47e9c774c1c09ead64942f577cf85b797bef8be1
MinimoMassimo <milkabiniam@gmail.com>
2022-11-08 11:43:14 +0300

    l

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ int len(char *str)
  */
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i;
+	int size1, size2, i, j;
 	char *nw;
 
 	if (s1 == NULL)
@@ -37,12 +37,16 @@ char *str_concat(char *s1, char *s2)
 	nw = malloc(size1 + size2 - 1);
 	if (nw == NULL)
 		return (NULL);
+	j = 0;
 	for (i = 0; i < (size1 + size2 - 1); i++)
 	{
 		if (i < size1)
 			nw[i] = s1[i];
 		else
-			nw[i] = s2[i - size1];
+		{
+			nw[i] = s2[j];
+			j++;
+		}
 	}
 	return (nw);
 }
