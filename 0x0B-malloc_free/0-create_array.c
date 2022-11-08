1a8a025cc8104711ea2f9235660e8b3e4a4f56eb
MinimoMassimo <milkabiniam@gmail.com>
2022-11-08 10:43:43 +0300

    l

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return ('\0');
 	str = malloc(size);
-	if (str == '\0')
+	if (str == NULL)
 		return ('\0');
 	for (i = 0; i < size; i++)
 	{
