d40920dd6dff3cdc45ef2a200f86de9b55a6cb8e
MinimoMassimo <milkabiniam@gmail.com>
2022-11-10 18:47:00 +0300

    l

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,9 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
-	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
-	for (i = 0; i < nmemb * size; i++)
-		ptr[i] = 0;
 	return (ptr);
 }
