3fcad78900e6d04e357aadd001103f6439b5cfc0
MinimoMassimo <milkabiniam@gmail.com>
2022-11-10 18:28:04 +0300

    l

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,60 +2,34 @@
 #include <stdlib.h>
 
 /**
- * len - length of string
- * @str: the string
- * Returns: the length
- */
-int len(char *str)
-{
-	int size;
-
-	for (size = 0; str[size]; size++)
-	;
-	return (size);
-}
-/**
- * _concat - just concatenates
- * @s1: first
- * @s2: second
- * Return: the new string
- */
-char *_cat(char *s1, char *s2, int n)
-{
-	int size1 = len(s1);
-	int i;
-
-	for(i = 0; i < n && s2[i]; i++)
-	{
-		s1[size1 + i] = s2[i];
-	}
-	return (s1);
-}
-/**
- * string_nconcat - concatenates two strings
+ * *string_nconcat - concatenates 2 strings
  * @s1: first string
  * @s2: second string
- * @n: number of bytes of s2 to add to s1
- * Return: concatenated string
+ * @n: num of chars we concatenate to s1 from s2
+ * Return: the concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int size2;
-	int size1;
 	char *ptr;
+	int size2, size1, i;
+	int a = (int) n;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	size2 = len(s2);
-	size1 = len(s1);
-	if (n >= (unsigned int) size2)
-		n = size2;
-	ptr = malloc(size1 + n + 1);
-	if (ptr == NULL)
+	for (size2 = 0; s2[size2]; size2++)
+		;
+	if (a >= size2)
+		a = size2;
+	for (size1 = 0; s1[size1]; size1++)
+		;
+	ptr = malloc(size1 + a + 1);
+	if(ptr == NULL)
 		return (NULL);
-	_cat(s1, s2, n);
-	_cat(ptr, "\0", 1);
+	for (i = 0; i < size1; i++)
+		ptr[i] = s1[i];
+	for (i = size1; i < size1 + a; i++)
+		ptr[i] = s2[i - size1];
 	return (ptr);
 }
