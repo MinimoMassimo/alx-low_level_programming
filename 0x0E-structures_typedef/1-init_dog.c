e6d682a08f8b5195c77289b369971f7117471a7c
MinimoMassimo <milkabiniam@gmail.com>
2022-11-14 20:43:44 +0300

    l

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * init_dog - initializes struct dog
+ * @d: object struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
