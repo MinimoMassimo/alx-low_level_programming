61b32f9075d4bee2a60831555b414833a3e250d6
MinimoMassimo <milkabiniam@gmail.com>
2022-11-14 20:29:15 +0300

    l

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - defines a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
