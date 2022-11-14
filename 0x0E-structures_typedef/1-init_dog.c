12614077099ec41baa5963b6f3d72f12b163948b
MinimoMassimo <milkabiniam@gmail.com>
2022-11-14 20:52:38 +0300

    l

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
+	struct dog a = *d;
+	a.name = name;
+	a.age = age;
+	a.owner = owner;
 }
