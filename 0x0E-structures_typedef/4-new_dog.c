4139faf1090ec1c38d34f49f03fc0e67ce852a43
MinimoMassimo <milkabiniam@gmail.com>
2022-11-15 21:12:00 +0300

    l

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *nwdog;
 	int sname, sowner;
 
+	nwdog = malloc(sizeof(*nwdog));
+	if (nwdog == NULL)
+	{
+		free (nwdog);
+		return (NULL);
+	}
 	for(sname = 0; name[sname]; sname++)
 	;
 	for(sowner = 0; owner[sowner]; sowner++)
 	;
+	nwdog->name = malloc(sname + 1);
+	nwdog->age = malloc((age + 1) * sizeof(int));
+	nwdog->owner = malloc(sowner + 1);
+	if (!nwdog->name || !nwdog->age || !nwdog->owner)
+	{
+		free(nwdog->name);
+		free(nwdog->age);
+		free(nwdog->owner);
+		free(nwdog);
+		return (NULL);
+	}
 	nwdog->name = name;
 	nwdog->age = age;
 	nwdog->owner = owner;
