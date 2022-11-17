a0217fe911652f6796bc9408cd6662b09a1878e9
MinimoMassimo <milkabiniam@gmail.com>
2022-11-17 14:27:48 +0300

    l

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums of all params
+ * @n: the first
+ * Return: the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, cint);
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+we're learning abour variadic functions and constant variables. Let's see how this goes
