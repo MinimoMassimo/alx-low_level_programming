648f272bbbfacd9198a5cbc78b3a884543bdfbe5
MinimoMassimo <milkabiniam@gmail.com>
2022-11-17 10:16:24 +0300

    l

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!size || !action)
+	if (!size || !action || !array)
 		return;
 	for (i = 0; i < size; i++)
 	{
