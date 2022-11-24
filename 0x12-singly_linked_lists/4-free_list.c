a18f90306431ca7ec9245f8c99b2fbf0907edf57
MinimoMassimo <milkabiniam@gmail.com>
2022-11-24 19:42:26 +0300

    l;

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: the start of the list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	if (head == NULL)
+		return;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	free(temp);
+	free_list(head);
+}
