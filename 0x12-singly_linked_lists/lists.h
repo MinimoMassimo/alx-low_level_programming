65471e71736624a395840b85380bef507ccf980a
MinimoMassimo <milkabiniam@gmail.com>
2022-11-24 20:09:46 +0300

    l

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,7 +18,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-void before_main() __attribute__((constructor));
+void before_main(void);
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
