1bf1544e9cd9cf98bf9675ca250b664e558d7bef
MinimoMassimo <milkabiniam@gmail.com>
2022-11-24 20:05:28 +0300

    l

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * before_main - prints before main
+ */
+void before_main()
+{
+	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,6 +18,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+void before_main() __attribute__((constructor));
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
