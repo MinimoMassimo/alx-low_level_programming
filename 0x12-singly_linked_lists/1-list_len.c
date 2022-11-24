edd3db016bdf21cc290a879e5397d9b22d27a88b
MinimoMassimo <milkabiniam@gmail.com>
2022-11-24 17:49:57 +0300

    l

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a linked list
+ * @h: the address of the linked list
+ * Return: returns the nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+		n++;
+	return n;
+}
