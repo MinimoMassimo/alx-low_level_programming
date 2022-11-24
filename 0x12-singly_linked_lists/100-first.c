f2156bcbd85094caf7baa6da98cbc3fc030ece7c
MinimoMassimo <milkabiniam@gmail.com>
2022-11-24 20:07:25 +0300

    l

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,8 @@
 /**
  * before_main - prints before main
  */
-void before_main()
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
