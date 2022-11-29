7011a6a769ff2a5fe6505f3fbe389049e4ea83f3
MinimoMassimo <milkabiniam@gmail.com>
2022-11-29 10:53:50 +0300

    l

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,15 +14,15 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL || head == NULL)
 		return (-1);
-	for (i = 0; i < index; i ++)
+	for (i = 0; i < index - 1; i++)
 	{
 		temp = temp->next;
 		if (temp == NULL)
 			return (-1);
 	}
-	cur  = temp-> next;
+	cur = temp->next;
 	temp->next = cur->next;
-	free (cur);
+	free(cur);
 	cur = NULL;
 	return (1);
 }
