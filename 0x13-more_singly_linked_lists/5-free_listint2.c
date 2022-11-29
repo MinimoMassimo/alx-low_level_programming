edefda49ed4485b95068e08d8271939dbdacce4d
MinimoMassimo <milkabiniam@gmail.com>
2022-11-29 09:36:23 +0300

    l

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ * free_listint - frees list
+ * @head: head of list
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+        listint_t *temp;
+
+        if (head == NULL)
+                return;
+        while (head != NULL)
+        {
+                temp = head;
+                head = head->next;
+                free(temp);
+        }
+}
+
+/**
  * free_listint2 - sets the head to null
  * @head: pointer to head
  * Return: nothing
