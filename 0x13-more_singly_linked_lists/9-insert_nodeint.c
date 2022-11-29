9f97f5c85657013d67a91c7c7ccedefe4eb3079a
MinimoMassimo <milkabiniam@gmail.com>
2022-11-29 10:05:39 +0300

    l

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *cur = *head;
 	listint_t *temp;
 
-	for (; i <= idx; i++)
+	for (; i < idx - 1; i++)
 	{
 		if (cur == NULL)
 			return (NULL);
