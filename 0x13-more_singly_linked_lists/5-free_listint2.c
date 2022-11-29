e36e96bb80b2c73cacb2a3945056ed7ebaa00a03
MinimoMassimo <milkabiniam@gmail.com>
2022-11-29 09:30:24 +0300

    l

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,9 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-
-	if (head == NULL || *head == NULL)
+	
+	if (head == NULL)
 		return;
-	while (*head != NULL)
-	{
-		temp = *head;
-		*head = (**head).next;
-		free(temp);
-	}
+	free_listint(*head);
+	*head = NULL;
 }
