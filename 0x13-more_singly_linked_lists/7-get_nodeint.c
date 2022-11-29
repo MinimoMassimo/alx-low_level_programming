d99f898336b41f85dbaaf1ba5514903d528bfed8
MinimoMassimo <milkabiniam@gmail.com>
2022-11-29 09:24:47 +0300

    l

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,5 +11,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	int i = 0;
 	listint_t *cur = head;
 
-	for (; i < index && cur != NULL; ; 
+	for (; i < index; i++)
+	{
+		cur = cur->next;
+		if (cur == NULL)
+			return (NULL);
+	}
+	return (cur);
+
 }
