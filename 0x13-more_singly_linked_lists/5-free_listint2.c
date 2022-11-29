dde20122cea78ff005a183b2d553702128f2ceec
MinimoMassimo <milkabiniam@gmail.com>
2022-11-29 09:40:44 +0300

    l

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,16 +7,16 @@
  */
 void free_listint(listint_t *head)
 {
-        listint_t *temp;
+	listint_t *temp;
 
-        if (head == NULL)
-                return;
-        while (head != NULL)
-        {
-                temp = head;
-                head = head->next;
-                free(temp);
-        }
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
 
 /**
