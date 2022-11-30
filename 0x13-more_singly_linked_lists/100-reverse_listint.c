c1db183d2053183a37fbdad2bf1d2c1cb109ebbb
MinimoMassimo <milkabiniam@gmail.com>
2022-11-30 09:16:45 +0300

    l

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a list
+ * @head: the head of the list
+ * Return: pointer to head of reversed list
+ */
+lsitint_t *reverse_listint(listint_t **head)
+{
+	listint_t *nuovo = *head;
+	listint_t *temp = (**head).next;
+
+	nuovo->next = NULL;
+	while (temp != NULL)
+	{
+		nuovo->next = nuovo;
+		nuovo->n = temp->n;
+		temp = temp->next;
+	}
+	return (nuovo);
+}
