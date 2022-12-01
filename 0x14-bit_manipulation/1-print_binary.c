4f3d69780df67f1a516c4bf6ed714f6fcdea1b8f
MinimoMassimo <milkabiniam@gmail.com>
2022-12-01 12:50:05 +0300

    k

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary rep of a num
+ * @n: the decimal num
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int index = 1;
+	int i = 0;
+	int ans = 0;
+
+	while (n / 2 > 0)
+	{
+		ans  += (n % 2) *  
+		n /= 2;
+	}
+}
