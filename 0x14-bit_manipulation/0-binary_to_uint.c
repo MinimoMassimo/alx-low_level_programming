834190dd5f5a77d674e117122e4a238aba8dd4f5
MinimoMassimo <milkabiniam@gmail.com>
2022-12-01 12:10:15 +0300

    l

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * exponent - a^b
+ * @a: base
+ * @b: the exponent
+ * Return: int
+ */
+unsigned int exponent(unsigned int a, unsigned int b)
+{
+	unsigned int ans = 1;
+	int i;
+
+	if (a == 0)
+		return (0);
+	if (b == 0)
+		return (1);
+	for (i = 1; i <= b; i++)
+		ans *= a;
+	return (ans);
+}
+
+/**
+ * binary_to_uint - converts binary num to an unsigned int
+ * @b: pointer to a string of 0s and 1s
+ * Return: converted number, or 0 if b has chars that are not 01 or b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ans = 0;
+	int count = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[count] != NULL)
+	{
+		if (b[count] != 0 && b[count] != 1)
+			return (0);
+		count++;
+	}
+	count -= 1;
+	for (; count >= 0; count--)
+	{
+		ans += b[count] * exponent(2, count); 
+	}
+	return (ans);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+Hello
+We're learning about bit manipulation
+This promises to be fun
