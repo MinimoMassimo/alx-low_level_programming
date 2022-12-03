74da373d0995fad29f7707a7335d9d7c2bc32ba2
MinimoMassimo <milkabiniam@gmail.com>
2022-12-03 05:54:27 +0300

    l

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * flip_bits - number of bits you need to flip to get from one num to another
+ * @n: num1
+ * @m: num2
+ * Return: num of bits difference
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (1);
+}
