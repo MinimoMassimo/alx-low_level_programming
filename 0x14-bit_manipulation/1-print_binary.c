1cf2445c469fbf5030ffa87a455e4f586ada078a
MinimoMassimo <milkabiniam@gmail.com>
2022-12-03 05:34:10 +0300

    l

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -16,15 +16,14 @@ void print_binary(unsigned long int n)
 		len++;
 		temp >>= 1;
 	}
-	temp = len;
+	len--;
 	while (len >= 0)
 	{
 		if ((n >> len) & 1)
 			_putchar('1');
 		else
 		{
-			if (len != temp && len >= 1)
-				_putchar('0');
+			_putchar('0');
 		}
 		len--;
 	}
