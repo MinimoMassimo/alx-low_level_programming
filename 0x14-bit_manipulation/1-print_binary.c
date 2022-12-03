be97a85b571b536f057d973b184bbf4f5e2dcc82
MinimoMassimo <milkabiniam@gmail.com>
2022-12-03 05:38:32 +0300

    l

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,20 +11,22 @@ void print_binary(unsigned long int n)
 	int temp = n;
 	int len = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	while (temp > 0)
 	{
 		len++;
 		temp >>= 1;
 	}
-	len--;
 	while (len >= 0)
 	{
 		if ((n >> len) & 1)
 			_putchar('1');
 		else
-		{
 			_putchar('0');
-		}
 		len--;
 	}
 }
