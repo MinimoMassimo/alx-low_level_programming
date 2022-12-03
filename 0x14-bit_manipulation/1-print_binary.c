4de42401cb1e31afc1cb94e38568f3fac641d493
MinimoMassimo <milkabiniam@gmail.com>
2022-12-03 05:23:50 +0300

    l

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,59 +1,27 @@
 #include "main.h"
 
-/**
- * exponent - a^b
- * @a: base
- * @b: the exponent
- * Return: int
- */
-unsigned int exponent(unsigned int a, unsigned int b)
-{
-        unsigned int ans = 1;
-        unsigned int i;
-
-        if (a == 0)
-                return (0);
-        if (b == 0)
-                return (1);
-        for (i = 1; i <= b; i++)
-		ans *= a;
-	return (ans);
-}
 
 /**
- * printing - prints in reverse
- * @ curr: the current
- * @size: size of the whole int
- * Return: nothing
- */
-void printing(int curr, int size)
-{
-	if (size <= 0)
-		return;
-	printing(curr / 2, size - 1);
-	_putchar((curr % 2) % 10 + '0');
-}
-
-/**
- * print_binary - prints binary rep of a num
- * @n: the decimal num
+ * print_binary - prints binary representation of a number
+ * @n: the number
  * Return: nothing
  */
 void print_binary(unsigned long int n)
 {
-	int size = 0;
-	int curr = (int)n;
-	unsigned int exp = 0;
+	int temp = n;
+	int len = 0;
 
-	if (n == 0 || n == 1)
+	while (temp > 0)
 	{
-		_putchar n  + '0');
-		return;
+		len++;
+		temp >>= 1;
 	}
-	while (exp < n)
+	while (len >= 0)
 	{
-		exp = exponent(2, size);
-		size++;
+		if (n >> len) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+		len--;
 	}
-	printing(curr, size);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: pointer to next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
