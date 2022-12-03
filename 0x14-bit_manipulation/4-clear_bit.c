d165b9ddb1c616e546f2e4af1469c4fa959a10fc
MinimoMassimo <milkabiniam@gmail.com>
2022-12-03 05:50:48 +0300

    l

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets value of bit to 0 at index
+ * @n: the number
+ * @index: the index
+ * Return: 1 if success, -1 otherwise
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int max_bit;
+	unsigned long int temp = 1;
+
+	max_bit = (sizeof(unsigned long int) * 8);
+	if (index > max_bit)
+		return (-1);
+	temp = ~(temp << index);
+	*n = (*n & temp);
+	return(1);
+}
