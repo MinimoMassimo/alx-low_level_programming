752fbbd47619f544dc1a1490e9df5f372422aad8
MinimoMassimo <milkabiniam@gmail.com>
2022-12-06 20:57:10 +0300

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,8 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	n_wr = write(fd, text_context, strlen(text_content));
-	if (n_wr == -1 || n_wr != strlen(text_content))
+	n_wr = write(fd, text_content, strlen(text_content));
+	if (n_wr == -1 || n_wr != (size_t) strlen(text_content))
 	{
 		close(fd);
 		return (-1);
