b01e98c330bdb552b441ca24a60036325ff35e1f
MBN <milkabiniam@gmail.com>
2023-01-13 15:02:23 +0300

    l

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (!head)
 		return (NULL);
-	new = malloc(sizeof(dlistint_s));
+	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
